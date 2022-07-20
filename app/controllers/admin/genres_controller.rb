6f2a61212c1aabf658078355435ee11fe9f38ad9
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-07-20 03:55:36 +0000

    [Add]nagano_cake

---
+++ app/controllers/admin/genres_controller.rb
@@ -0,0 +1,38 @@
+class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+
+    @genre_name = Genre.find(params[:id])
+    @item = @ganre_name.item
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to admin_genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
+  def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to admin_genres_path
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :items, only: [:new, :index, :create, :show, :edit, :update, :destroy]
+  end
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+  end
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
 
-
-  devise_for :admins,skip: [:registrations, :passwords], controllers: {
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
