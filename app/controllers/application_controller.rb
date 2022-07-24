e05c4063d6c47c31bcb34549870585008deda0bd
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-07-24 13:48:44 +0000

    [Add]nagano_cake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  def after_sign_out_path_for(resource)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: 'about'
+  end
+
   namespace :admin do
+    root to: 'homes#top'
+    resources :genres,    only: [:index, :create, :edit, :update, :destroy]
+    resources :items,     only: [:new, :index, :create, :show, :edit, :update, :destroy]
     resources :customers, only: [:index, :show, :edit, :update]
   end
 
-  namespace :admin do
-    resources :items, only: [:new, :index, :create, :show, :edit, :update, :destroy]
-  end
-
-  namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update, :destroy]
-  end
-
   devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
   }
 
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions'
   }
 
 
