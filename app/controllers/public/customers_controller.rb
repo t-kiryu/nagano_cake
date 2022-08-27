cd3d6edecbbc2dc7a598b3ec56b356561114b2f2
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-08-27 05:32:32 +0000

    ストロングパラメータ更新

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -37,7 +37,7 @@ class Public::CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
-                                                 :postal_code, :address, :telephone_number, :is_deleted)
+                                                 :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 
 end
